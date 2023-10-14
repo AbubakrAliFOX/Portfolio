517388184693ce57f11f90795766838a5de00659
Abubakr <thefox5000@gmail.com>
2023-10-14 20:39:32 +0300

    Add Skills & Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/HeroSection';
 import Skills from './components/Skills';
 import Education from './components/Education';
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const Body = styled.div`
@@ -32,7 +34,12 @@ function App() {
           <Hero/>
           <Wrapper>
             <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
             <Education />
+            {/* <Contact /> */}
           </Wrapper>
         </Body>
       </Router>

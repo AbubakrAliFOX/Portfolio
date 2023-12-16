fac6656b2e3eaccc04bfe936b3f93d23a9ec5f9b
Abubakr <thefox5000@gmail.com>
2023-12-16 09:51:54 +0300

    :x: Remove footer nav links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -91,13 +91,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Nav>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href="#skills">Skills</NavLink>
-          <NavLink href="#experience">Experience</NavLink>
-          <NavLink href="#projects">Projects</NavLink>
-          <NavLink href="#education">Education</NavLink>
-        </Nav>
+        
         <SocialMediaIcons>
           <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
           <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>

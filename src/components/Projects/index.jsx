c6978d0d106641606551b78d0e65e91c6a347773
Abubakr <thefox5000@gmail.com>
2024-02-21 08:43:43 +0300

    :gear: Remove toggle feature

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import {
   Container,
   Wrapper,
@@ -16,12 +16,12 @@ import { projects } from "../../data/constants";
 const reversedProjects = projects.reverse();
 
 const Projects = ({ openModal, setOpenModal }) => {
-  const [toggle, setToggle] = useState('all');
+  // const [toggle, setToggle] = useState('all');
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
-        <ToggleButtonGroup >
+        {/* <ToggleButtonGroup >
           {toggle === 'all' ?
             <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
             :
@@ -45,7 +45,7 @@ const Projects = ({ openModal, setOpenModal }) => {
             :
             <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
           }
-        </ToggleButtonGroup>
+        </ToggleButtonGroup> */}
         <CardContainer>
           {reversedProjects.map((project, idx) => (
             <ProjectCard
